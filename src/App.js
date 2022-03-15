c841ee115e089493c6ef0b793e524b46d4f2d149
James F. Thomas <james.thomas@dxc.com>
2022-03-15 10:08:55 -0500

    "Initial commit"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [width, setWindowWidth] = useState(0);
+
+  const [height, setWindowHeight] = useState(0);
+
+
+  const updateWindow = () => {
+
+    const iWidth = window.document.body.clientWidth
+
+    const iHeight = window.innerHeight
+
+    setWindowWidth(iWidth)
+
+    setWindowHeight(iHeight)
+
+  }
+
+  useEffect(() => {
+
+    updateWindow();
+
+    window.addEventListener('resize', updateWindow)
+
+    return () =>
+      window.removeEventListener('resize', updateWindow)
+
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container" style={{ 'height': height, 'width': width, 'background-color': 'aqua' }}>
+      <h1> {`Container // width: ${width}px & height: ${height}px`} </h1>
+
     </div>
   );
 }
